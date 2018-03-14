c405b5d1243fffe034361a4abb0fe8f9c90f87ca
RV <vignesh-raghavan@users.noreply.github.com>
2018-03-13 22:33:16 -0400

    Add files via upload

--- jacobi/step6/jacobi6.cu
+++ jacobi/step6/jacobi6.cu
@@ -43,44 +43,6 @@ void readFile(char* fname, int* N, int* iter, float** A, float** b)
 }
 
 
-void jacobi(float* A, float* b, int N, int iter, float** x)
-{
-   int i, j, k;
-   float t;
-   float *y;
-
-   *x = (float*) malloc(N*sizeof(float));
-   y = (float*) malloc(N*sizeof(float));
-   for(i = 0; i < N; i++)
-   {
-      (*x)[i] = 0.0; // Initial Guess
-   }
-
-   for(k = 0; k < iter; k++)
-   {
-      for(i = 0; i < N; i++)
-      {
-         t = 0;
-         for(j = 0; j < N; j++)
-         {
-            if(i != j)
-            {
-               t += ((A[ ((N*i)+j) ]) * ((*x)[j]));
-            }
-         }
-         y[i] = ((b[i]) - t)/(A[ ((N*i)+i) ]);
-         //printf("k %02d i %02d t %f y %f\n", k, i, t, y[i]);
-      }
-
-      for(i = 0; i < N; i++)
-      {
-         (*x)[i] = y[i];
-      }
-   }
-   free(y);
-}
-
-
 
 
 __global__
@@ -107,17 +69,6 @@ void iloop(float* A, float* b, int N, float* x, float* y, int offset, int stream
 	}
 }
 
-__global__
-void add(float* a, float* b, int N)
-{
-	for(int i = 0; i < N; i++)
-	{
-		b[i] = a[i] + b[i];
-	}
-}
-
-
-
 
 int main(int argc, char* argv[])
 {
@@ -140,7 +91,7 @@ int main(int argc, char* argv[])
 	float* y;
    float* c;
    char* fname;
-   int N, iter, i, j;
+   int N, iter, i, j, M;
 
    cudaEvent_t start, stop;
    cudaEventCreate(&start);
@@ -148,6 +99,8 @@ int main(int argc, char* argv[])
 
    if(argc >= 2) fname = argv[1];
    else fname = "../inputs/8.txt";
+   if(argc >= 3) M = atoi(argv[2]);
+   else M = 32;
 
 	//if(argc >= 3) devID = atoi(argv[2]);
 	//else devID = 1;
@@ -184,7 +137,7 @@ int main(int argc, char* argv[])
 
 	nstreams = 2;
 	streamsize = N/nstreams;
-	blocksize = (int) fmin(256, streamsize);
+	blocksize = (int) fmin(M, streamsize);
 	numblocks = streamsize/blocksize;
 	//numblocks = (N+blocksize-1)/blocksize;
 	printf("CUDA : Streams %d, Grid Size %d, Block size %d\n", nstreams, numblocks, blocksize);
@@ -203,11 +156,6 @@ int main(int argc, char* argv[])
 		for(i = 0; i < nstreams; i++)
 		{
 			int offset = i*streamsize;
-			//if( cudaMemcpyAsync(&d_x[offset], &d_y[offset], streamsize*sizeof(float), cudaMemcpyDeviceToDevice, streams[i])!= cudaSuccess ) printf("CUDA : memory copy error!\n");
-		//}
-		//for(i = 0; i < nstreams; i++)
-		//{
-		//	int offset = i*streamsize;
 			iloop<<< numblocks, blocksize, 0, streams[i] >>>(d_A, d_b, N, d_x, d_y, offset, streamsize); // kernel launch on GPU
 			iloop<<< numblocks, blocksize, 0, streams[i] >>>(d_A, d_b, N, d_y, d_x, offset, streamsize); // kernel launch on GPU
 		}
