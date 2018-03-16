5b4edb73c380cecd40bba8347f913c8b0d84d0d4
RV <vignesh-raghavan@users.noreply.github.com>
2018-03-16 19:51:21 -0400

    Update step9.sh

--- jacobi/step9/step9.sh
+++ jacobi/step9/step9.sh
@@ -4,22 +4,22 @@ rm -f 32.o 64.o 128.o 256.o
 rm -rf nvprof
 mkdir nvprof
 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.8    ./jacobi9 ../inputs/8.txt    32 >> 32.o 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.256  ./jacobi9 ../inputs/256.txt  32 >> 32.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.1024 ./jacobi9 ../inputs/1024.txt 32 >> 32.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.5120 ./jacobi9 ../inputs/5120.txt 32 >> 32.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.8.o    ./jacobi9 ../inputs/8.txt    32 >> 32.o 
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.256.o  ./jacobi9 ../inputs/256.txt  32 >> 32.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.1024.o ./jacobi9 ../inputs/1024.txt 32 >> 32.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/32.5120.o ./jacobi9 ../inputs/5120.txt 32 >> 32.o
 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.8    ./jacobi9 ../inputs/8.txt    64 >> 64.o 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.256  ./jacobi9 ../inputs/256.txt  64 >> 64.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.1024 ./jacobi9 ../inputs/1024.txt 64 >> 64.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.5120 ./jacobi9 ../inputs/5120.txt 64 >> 64.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.8.o    ./jacobi9 ../inputs/8.txt    64 >> 64.o 
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.256.o  ./jacobi9 ../inputs/256.txt  64 >> 64.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.1024.o ./jacobi9 ../inputs/1024.txt 64 >> 64.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/64.5120.o ./jacobi9 ../inputs/5120.txt 64 >> 64.o
 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.8    ./jacobi9 ../inputs/8.txt    128 >> 128.o 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.256  ./jacobi9 ../inputs/256.txt  128 >> 128.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.1024 ./jacobi9 ../inputs/1024.txt 128 >> 128.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.5120 ./jacobi9 ../inputs/5120.txt 128 >> 128.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.8.o    ./jacobi9 ../inputs/8.txt    128 >> 128.o 
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.256.o  ./jacobi9 ../inputs/256.txt  128 >> 128.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.1024.o ./jacobi9 ../inputs/1024.txt 128 >> 128.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/128.5120.o ./jacobi9 ../inputs/5120.txt 128 >> 128.o
 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.8    ./jacobi9 ../inputs/8.txt    256 >> 256.o 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.256  ./jacobi9 ../inputs/256.txt  256 >> 256.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.1024 ./jacobi9 ../inputs/1024.txt 256 >> 256.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.5120 ./jacobi9 ../inputs/5120.txt 256 >> 256.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.8.o    ./jacobi9 ../inputs/8.txt    256 >> 256.o 
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.256.o  ./jacobi9 ../inputs/256.txt  256 >> 256.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.1024.o ./jacobi9 ../inputs/1024.txt 256 >> 256.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/256.5120.o ./jacobi9 ../inputs/5120.txt 256 >> 256.o
