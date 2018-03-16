9939833f925d7f025800b6ec6e780eb79a0672d9
RV <vignesh-raghavan@users.noreply.github.com>
2018-03-16 19:45:36 -0400

    Update step8.sh

--- jacobi/step8/step8.sh
+++ jacobi/step8/step8.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.8    ./jacobi8 ../inputs/8.txt    >> 1024.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.256  ./jacobi8 ../inputs/256.txt  >> 1024.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.1024 ./jacobi8 ../inputs/1024.txt >> 1024.o
-CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.5120 ./jacobi8 ../inputs/5120.txt >> 1024.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.8.o    ./jacobi8 ../inputs/8.txt    >> 1024.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.256.o  ./jacobi8 ../inputs/256.txt  >> 1024.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.1024.o ./jacobi8 ../inputs/1024.txt >> 1024.o
+CUDA_VISIBLE_DEVICES=1 nvprof --log-file nvprof/1024.5120.o ./jacobi8 ../inputs/5120.txt >> 1024.o
